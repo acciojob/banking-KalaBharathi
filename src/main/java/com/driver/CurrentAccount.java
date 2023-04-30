2aec3b4e193f73e62d377f2624ee1b68ae343402
unknown <yeruvakala28@gmail.com>
2023-04-30 16:02:08 +0530

    Final commit

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,22 +1,21 @@
 package com.driver;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Locale;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.max;
 
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
-    public static int minBalance=5000;
-
+    public static int minBalance = 5000;
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
-        // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-        super(name,balance,minBalance);
-        if(balance<minBalance){
+        super(name, balance, minBalance);
+        if(balance < minBalance) {
             throw new Exception("Insufficient Balance");
         }
-        this.tradeLicenseId=tradeLicenseId;
+        this.tradeLicenseId = tradeLicenseId;
         validateLicenseId();
+        // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
     }
 
     public String getTradeLicenseId() {
@@ -28,41 +27,64 @@ public class CurrentAccount extends BankAccount{
     }
 
     public void validateLicenseId() throws Exception {
+        if(!tradeLicenseId.equals(tradeLicenseId.toUpperCase())) {
+            throw new Exception("Valid License can not be generated");
+        }
+        int size = tradeLicenseId.length();
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        getFrequencyMap(size, map);
+        //odd even
+        if(Collections.max(map.values()) > size/2) {
+            throw new Exception("Valid License can not be generated");
+        }
+
+        while (!isValidTradeId()) {
+            List<Character> list = tradeLicenseId.chars()
+                    .mapToObj(e -> (char)e)
+                    .collect(Collectors.toList());
+            Collections.shuffle(list);
+            StringBuilder sb = new StringBuilder();
+
+            // Appends characters one by one
+            for (Character ch : list) {
+                sb.append(ch);
+            }
+
+            String tradeId = sb.toString();
+            this.tradeLicenseId = tradeId;
+        }
+
+        return;
+
         // A trade license Id is said to be valid if no two consecutive characters are same
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
-        if(!tradeLicenseId.equals(tradeLicenseId.toUpperCase())){
-            throw new Exception("Valid License can not be generated");
-        }
-        HashMap<Character,Integer> hm=new HashMap<>();
-        int n=tradeLicenseId.length();
-        for(int i=0;i<n;i++){
-            hm.put(tradeLicenseId.charAt(i),hm.getOrDefault(tradeLicenseId.charAt(i),0)+1);
-        }
-        int maxFre=Collections.max(hm.values());
-        if((maxFre>(n+1)/2) || (maxFre>n/2)){
-            throw new Exception("Valid License can not be generated");
-        }
-        while(!isValidId(tradeLicenseId)) {
-            tradeLicenseId=rearrange(tradeLicenseId);
-        }
-            return;
     }
-    private Boolean isValidId(String tradeLicenseId){
-        int n=tradeLicenseId.length();
-        for (int i = 0; i < n - 1; i++) {
-            if (tradeLicenseId.charAt(i) == tradeLicenseId.charAt(i + 1)) {
-               return false;
+
+    private void getFrequencyMap(int size, HashMap<Character, Integer> map) {
+        for(int i = 0; i< size; i++) {
+            if(map.containsKey(tradeLicenseId.charAt(i))) {
+                int value = map.get(tradeLicenseId.charAt(i));
+                map.put(tradeLicenseId.charAt(i), value+1 );
+            } else {
+                map.put(tradeLicenseId.charAt(i), 1);
             }
         }
-        return true;
     }
-    private String rearrange(String tradeLicenseId){
-        char[] chars = tradeLicenseId.toCharArray();
-        Collections.shuffle(Arrays.asList(chars));
-        tradeLicenseId = new String(chars);
-        return tradeLicenseId;
+
+    private boolean isValidTradeId() {
+        int size = tradeLicenseId.length();
+        for(int i = 0, j = 1; i< size && j< size; i++, j++) {
+            if(((Character)tradeLicenseId.charAt(i)).equals((Character) tradeLicenseId.charAt(j))) {
+                break;
+            }
+            if(j== size -1) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
