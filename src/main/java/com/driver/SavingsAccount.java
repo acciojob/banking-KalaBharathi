74321e79fc26e297efc8d0c0218de45d76154cc4
unknown <yeruvakala28@gmail.com>
2023-04-30 14:30:41 +0530

    secondCommit

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -57,7 +57,7 @@ public class SavingsAccount extends BankAccount{
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-        return super.getBalance()*Math.pow(1+rate/times,years);
+        return super.getBalance()*Math.pow(1+rate/times,times*years);
     }
 
 }
